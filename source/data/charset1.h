3135cf16a1189ea538968f9f5e81d12fda3bef0e
evanbowman <ebowman@bu.edu>
2021-09-10 13:30:35 -0400

    add russian characters, edit readme

--- source/data/charset1.h
+++ source/data/charset1.h
@@ -3,13 +3,13 @@
 
 //======================================================================
 //
-//	charset1, 9520x8@4, 
+//	charset1, 10032x8@4, 
 //	Transparent color : FF,00,FF
 //	+ palette 256 entries, not compressed
-//	+ 1190 tiles not compressed
-//	Total size: 512 + 38080 = 38592
+//	+ 1254 tiles not compressed
+//	Total size: 512 + 40128 = 40640
 //
-//	Time-stamp: 2021-09-10, 12:51:45
+//	Time-stamp: 2021-09-10, 13:24:10
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_CHARSET1_H
 #define GRIT_CHARSET1_H
 
-#define charset1TilesLen 38080
-extern const unsigned int charset1Tiles[9520];
+#define charset1TilesLen 40128
+extern const unsigned int charset1Tiles[10032];
 
 #define charset1PalLen 512
 extern const unsigned short charset1Pal[256];
